b4269cb567a74119072017ac0603d3fef6b1b35c
aidanohart <44532573+aidanohart@users.noreply.github.com>
2024-11-13 15:27:52 +0000

    resolution 1080x1080

--- server.js
+++ server.js
@@ -26,26 +26,24 @@ try {
 			},
 			async (err, fileName) => {
 				let image = await imagejs.Image.load(fileName);
-				const coppedImage = image.crop({ x: image.width / 4.5, width: image.height });
+				const croppedImage = image.crop({ x: image.width / 4.5, width: image.height });
 
-				image.data = coppedImage.data;
-				image.width = coppedImage.width;
-				image.height = coppedImage.height;
+				const resizedImage = croppedImage.resize({ width: 1080, height: 1080 });
 
-				for (let x = 0; x < image.width; x++) {
-					for (let y = 0; y < image.height; y++) {
-						const pixelArr = image.getPixelXY(x, y);
+				for (let x = 0; x < resizedImage.width; x++) {
+					for (let y = 0; y < resizedImage.height; y++) {
+						const pixelArr = resizedImage.getPixelXY(x, y);
 						const r = pixelArr[0];
 						const g = pixelArr[1];
 						const b = pixelArr[2];
 
 						if (g > r + b) {
-							image.setPixelXY(x, y, [255, 255, 255, 0]);
+							resizedImage.setPixelXY(x, y, [255, 255, 255, 0]);
 						}
 					}
 				}
 
-				image.save(fileName);
+				await resizedImage.save(fileName);
 			}
 		);
 	});
